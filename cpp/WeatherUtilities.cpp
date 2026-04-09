c46b2cd9b7558dd7d8db24ff0edc3e2fe65d619e
Franco Nogarin <spydmobile@gmail.com>
2026-04-09 16:16:37 -0600

    Lawson trace: filter to growth time range, show secs+rh on output

--- cpp/WeatherUtilities.cpp
+++ cpp/WeatherUtilities.cpp
@@ -356,17 +356,21 @@ HRESULT WeatherUtilities::GetCalculatedIFWIValues(ICWFGM_GridEngine *gridEngine,
 						return hr;
 					}
 					// compute spatially interpolated instantaneous FWI values for the current time
-					{ static int _lc=0; if(_lc<3){_lc++;
+					{
+					unsigned long _s=(unsigned long)secs.GetTotalSeconds();
+					static int _lc=0, _lo=0;
+					if(_lc<10 && _s>=39600 && _s<=50400){_lc++;
 					  fprintf(stderr,"[LAWSON-IN#%d] p_dFFMC=%.4f t_dFFMC=%.4f precip=%.4f temp=%.4f rh0=%.4f rh=%.4f rh1=%.4f ws=%.4f secs=%lu\n",
-					    _lc, p_dfwi.dFFMC, t_dfwi.dFFMC, wx->Precipitation, wx->Temperature, wh0.RH, wx->RH, wh1.RH, wx->WindSpeed, (unsigned long)secs.GetTotalSeconds());
-					  fflush(stderr);}}
-					if (FAILED(hr = m_fwi.HourlyFFMC_Lawson_Contiguous(p_dfwi.dFFMC, t_dfwi.dFFMC, wx->Precipitation, wx->Temperature, wh0.RH, wx->RH, wh1.RH, wx->WindSpeed, (unsigned long)secs.GetTotalSeconds(), &ifwi->FFMC)))
+					    _lc, p_dfwi.dFFMC, t_dfwi.dFFMC, wx->Precipitation, wx->Temperature, wh0.RH, wx->RH, wh1.RH, wx->WindSpeed, _s);
+					  fflush(stderr);}
+					if (FAILED(hr = m_fwi.HourlyFFMC_Lawson_Contiguous(p_dfwi.dFFMC, t_dfwi.dFFMC, wx->Precipitation, wx->Temperature, wh0.RH, wx->RH, wh1.RH, wx->WindSpeed, _s, &ifwi->FFMC)))
 					{
 						weak_assert(false);
 						return hr;
 					}
-					{ static int _lo=0; if(_lo<3){_lo++;
-					  fprintf(stderr,"[LAWSON-OUT#%d] FFMC=%.6f\n", _lo, ifwi->FFMC);fflush(stderr);}}
+					if(_lo<10 && _s>=39600 && _s<=50400){_lo++;
+					  fprintf(stderr,"[LAWSON-OUT#%d] secs=%lu FFMC=%.6f rh=%.4f\n", _lo, _s, ifwi->FFMC, wx->RH);fflush(stderr);}
+					}
 					if (ifwi->FFMC != ofwi.FFMC)
 						ifwi->SpecifiedBits |= IFWIDATA_OVERRODE_FFMC;
 					break;
